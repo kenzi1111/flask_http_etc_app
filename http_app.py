931c0980e24c9dde1a9bffa8baea82b714e4b3cc
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2023-10-07 10:57:21 +0900

    form 修正

--- http_app.py
+++ http_app.py
@@ -1,22 +1,23 @@
-from flask import Flask,request
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 # ログイン機能
-@app.route("/login",methods=["GET","POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     # リクエストメソッドの使用
     if request.method == "GET":
-        return '''
+        return """
         <form action="/login" method="POST">
             password:<input type="text"><br>
             <input type="submit">
         </form>
-        '''
-    
+        """
+
     # リクエストメソッドPOSTの時は以下の処理を行う
     return "Logged in"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000,debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- template_app.py
+++ template_app.py
@@ -1,15 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     # return "Hello from index"
     # index.htmlをレンダリングする
     return render_template("index.html")
+
+
 @app.route("/user/<name>")
 def hello(name):
-    return render_template("hello.html",name = name)
+    return render_template("hello.html", name=name)
 
 
 if __name__ == "__main__":
