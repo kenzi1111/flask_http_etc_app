459a21885c74ca89c4be57713415d6cb800b808a
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2023-10-07 10:02:34 +0900

    Add http_app.py

---
+++ http_app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import Flask,request
+
+app = Flask(__name__)
+
+# ログイン機能
+@app.route("/login",methods=["GET","POST"])
+def login():
+    # リクエストメソッドを使用
+    print(request.method)
+    return "login page"
+
+if __name__ == "__main__":
+    app.run(port=8000,debug=True)
