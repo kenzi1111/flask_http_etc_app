36cc684648646072409d6618e0d77a7d6680c779
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2023-10-07 10:55:52 +0900

    flask practice

--- http_app.py
+++ http_app.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask import Flask,request
 
 app = Flask(__name__)
@@ -6,9 +5,18 @@ app = Flask(__name__)
 # ログイン機能
 @app.route("/login",methods=["GET","POST"])
 def login():
-    # リクエストメソッドを使用
-    print(request.method)
-    return "login page"
+    # リクエストメソッドの使用
+    if request.method == "GET":
+        return '''
+        <form action="/login" method="POST">
+            password:<input type="text"><br>
+            <input type="submit">
+        </form>
+        '''
+    
+    # リクエストメソッドPOSTの時は以下の処理を行う
+    return "Logged in"
+
 
 if __name__ == "__main__":
-    app.run(port=8000,debug=True)
+    app.run(host="0.0.0.0",port=8000,debug=True)
